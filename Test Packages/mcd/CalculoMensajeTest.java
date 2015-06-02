eb3f44348c82b4ee9319d5f90b96eb2ee575a2ae
oquintansocampo <oquintansocampo@atanasoft06.danielcastelao.org>
2015-06-02 11:31:20 +0200

    TEST MENSAJES: Declaramos resultadoesperado como un String. (resultados posibles { Correcto, ERROR exception, ERROR negativo}) Llamamos a los metodos setNumero() y al metodo obtenerMCD() con los numeros a testear Declaramos resultado como un String y le damos el valor de getMensajeResultado() Para negativo ponemos uno o los dos numeros negativos y al hacer el setNum() el mensaje se cambiara a ERROR negativo. Para exception dividimos entre 0, el mensaje se cambiara a ERROR exception.

--- Test Packages/mcd/CalculoMensajeTest.java
+++ Test Packages/mcd/CalculoMensajeTest.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package mcd;
 
 import org.junit.AfterClass;
@@ -16,42 +15,16 @@ import static org.junit.Assert.*;
  * @author oquintansocampo
  */
 public class CalculoMensajeTest {
-    
+
     public CalculoMensajeTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
-    }
-
-    /**
-     * Test of setNumero1 method, of class Calculo.
-     */
-    @Test
-    public void testSetNumero1() {
-        System.out.println("setNumero1");
-        int numero = 0;
-        Calculo instance = new Calculo();
-        instance.setNumero1(numero);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setNumero2 method, of class Calculo.
-     */
-    @Test
-    public void testSetNumero2() {
-        System.out.println("setNumero2");
-        int numero = 0;
-        Calculo instance = new Calculo();
-        instance.setNumero2(numero);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -62,12 +35,13 @@ public class CalculoMensajeTest {
         System.out.println("obtenerMCD");
         int num1 = 0;
         int num2 = 0;
-        Calculo instance = new Calculo();
-        int expResult = 0;
-        int result = instance.obtenerMCD(num1, num2);
+        String expResult = "ERROR exception";
+        Calculo instance = new Calculo(); 
+        instance.setNumero1(num1);
+        instance.setNumero2(num2);
+        instance.obtenerMCD(num1, num2);
+        String result = instance.getMensajeResultado();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-    
+
 }
